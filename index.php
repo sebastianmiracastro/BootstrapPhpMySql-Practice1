1743756d80ed91744a72811bb54828273c84caa6
sebastianmiracastro <smira44@misena.edu.co>
2022-01-24 19:49:29 -0500

    Started New Proyect

---
+++ index.php
@@ -0,0 +1,56 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Sebastian Mira Castro">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Personal Contact Agenda - Main</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sidebars/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <!-- Custom styles for this template -->
+    <link href="sidebars.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<main>
+  <nav class="navbar  justify-content-between navbar-expand-md navbar-dark bg-dark fixed-top">
+    <div class="dropdown">
+      <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="./images/kiro.jpeg" alt="" width="32" height="32" class="rounded-circle me-2">
+        <strong class="navbar-light navbar-nav nav-link">Sebastian Mira Castro</strong>
+      </a>
+      <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+        <li><a class="dropdown-item " href="https://www.facebook.com/profile.php?id=100008382820923">Facebook</a></li>
+        <li><a class="dropdown-item" href="">WhatsApp</a></li>
+        <li><a class="dropdown-item" href="https://github.com/sebastianmiracastro">GitHub</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Contactos</a></li>
+      </ul>
+    </div>
+    <a class="navbar-brand" href="#">Personal Contacts</a> 
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-inline mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="sumbit">Search</button>
+    </form>
+</main>
+<div class="Container">
+  <h1 class="page-header text-center">Contacts</h1>
+</div>
+
+  </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="sidebars.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+</html>
