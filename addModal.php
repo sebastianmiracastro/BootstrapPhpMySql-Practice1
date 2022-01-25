6e6542166488902e03ce2ee95862ff940f06a648
sebastianmiracastro <smira44@misena.edu.co>
2022-01-24 20:50:09 -0500

    modal windows added

---
+++ addModal.php
@@ -0,0 +1,13 @@
+<div class="modal fade" id="addNew" tabindex="-1"
+role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <center> <h4 class="modal-title">Add Contact</h4> </center>
+                <button type="button" class="close" data-dismess="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,28 +1,16 @@
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Sebastian Mira Castro">
-    <meta name="generator" content="Hugo 0.88.1">
-    <title>Personal Contact Agenda - Main</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sidebars/">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    
-
-    <!-- Bootstrap core CSS -->
-<link href="/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
-    <!-- Custom styles for this template -->
-    <link href="sidebars.css" rel="stylesheet">
-  </head>
-  <body>
-    
-<main>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Sebastian Mira Castro">
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="bootstrap/css/custom.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+  <title>Personal Contact Agenda - Main</title>
+</head>
+<body>
   <nav class="navbar  justify-content-between navbar-expand-md navbar-dark bg-dark fixed-top">
     <div class="dropdown">
       <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
@@ -34,7 +22,7 @@
         <li><a class="dropdown-item" href="">WhatsApp</a></li>
         <li><a class="dropdown-item" href="https://github.com/sebastianmiracastro">GitHub</a></li>
         <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Contactos</a></li>
+        <li><a class="dropdown-item" style="cursor: no-drop;" href="#">Contact me</a></li>
       </ul>
     </div>
     <a class="navbar-brand" href="#">Personal Contacts</a> 
@@ -42,15 +30,34 @@
       <input class="form-inline mr-sm-2" type="text" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="sumbit">Search</button>
     </form>
-</main>
-<div class="Container">
-  <h1 class="page-header text-center">Contacts</h1>
-</div>
+  </nav>
+  <div  class="container">
+    <h1 class="page-header text-center">Personal Contact Agenda</h1>
+    <div class="row">
+      <div class="col-sm-12">
+        <a href="#addNew" class="btn btn-primary" data-toggle="modal">New Contact <span class="fa fa-plus"></span></a>
+      </div>
+        <table class="table table-bordered table-striped" style="margin-top:20px; " >
+          <thead>
+            <th>ID</th>
+            <th>Contact Name</th>
+            <th>Phone Number</th>
+            <th>Mail</th>
+            <th>Address</th>
+            <th>Actions</th>
+          </thead>
+          <tbody>
 
-  </body>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  <script src="sidebars.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <?php include('addModal.php'); ?>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="sidebars.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 </html>
